88a524ee744e5824a4cece1afab6810a4a513fdd
EvelinaVirda <for.alvi.development@gmail.com>
2020-07-02 09:00:18 +0700

    navbar

--- application/views/pages/template/navbar.php
+++ application/views/pages/template/navbar.php
@@ -1,30 +1,29 @@
 <div class="container">
-        <header>
-            <img src="images/logo.svg" alt="23andfour logo" class="logo">
+    <header>
+        <img src="images/logo.svg" alt="23andfour logo" class="logo">
 
-            <nav>
-                <a href="#" class="hide-desktop">
-                    <img src="<?= base_url('assets/pages/') ?>images/ham.svg" alt="toggle menu" class="menu" id="menu">
-                </a>
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" href="<?= base_url('indexi'); ?>">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">News</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Product</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= base_url('indexi/about'); ?>">About</a>
+            </li>
+        </ul>
+    </header>
 
-                <ul class="show-desktop hide-mobile" id="nav">
-                    <li id="exit" class="exit-btn hide-desktop">
-                        <img src="<?= base_url('assets/pages/') ?>images/exit.svg" alt="exit menu">
-                    </li>
-                    <li><a href="#">home</a></li>
-                    <li><a href="#">services</a></li>
-                    <li><a href="#">about</a></li>
-                    <li><a href="#">contact</a></li>
-                </ul>
-            </nav>
-        </header>
+    <section>
+        <img src="<?= base_url('assets/pages/') ?>images/server.svg" alt="server graphic" class="server">
 
-        <section>
-            <img src="<?= base_url('assets/pages/') ?>images/server.svg" alt="server graphic" class="server">
-            
-            <h1>Web Hosting for the Modern Era</h1>
-            <p class="subhead">Go serverless and pay only for what you use.</p>
+        <h1>Web Hosting for the Modern Era</h1>
+        <p class="subhead">Go serverless and pay only for what you use.</p>
 
-            <img src="<?= base_url('assets/pages/') ?>images/scroll.svg" alt="scroll down" class="scroll hide-mobile show-desktop">
-        </section>
-    </div>
+        <img src="<?= base_url('assets/pages/') ?>images/scroll.svg" alt="scroll down" class="scroll hide-mobile show-desktop">
+    </section>
+</div>
