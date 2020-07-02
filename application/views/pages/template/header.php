3df952e62faa34d08e20ba63e5141f5bea22293a
EvelinaVirda <for.alvi.development@gmail.com>
2020-07-02 11:56:35 +0700

    nav

--- application/views/pages/template/header.php
+++ application/views/pages/template/header.php
@@ -6,10 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Frontend Dev Course</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?= base_url('assets/pages/') ?>css/main.css">
 	<link rel="stylesheet" href="<?= base_url('assets/pages/') ?>css/news.css">
 </head>
--- application/views/pages/template/navbar.php
+++ application/views/pages/template/navbar.php
@@ -1,19 +1,20 @@
 <div class="container">
-    <header>
-        <img src="images/logo.svg" alt="23andfour logo" class="logo">
+ <header>
+    <img src="images/logo.svg" alt="23andfour logo" class="logo">
 
-        <ul class="nav justify-content-end">
-            <li class="nav-item">
-                <a class="nav-link active" href="<?= base_url('indexi'); ?>">Home</a>
+    <nav>
+        <a href="#" class="hide-desktop">
+            <img src="<?= base_url('assets/pages/') ?>images/ham.svg" alt="toggle menu" class="menu" id="menu">
+        </a>
+
+        <ul class="show-desktop hide-mobile" id="nav">
+            <li id="exit" class="exit-btn hide-desktop">
+                <img src="<?= base_url('assets/pages/') ?>images/exit.svg" alt="exit menu">
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= base_url('indexi/news'); ?>">News</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Product</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= base_url('indexi/about'); ?>">About</a>
-            </li>
+            <li><a href="<?= base_url('indexi'); ?>">home</a></li>
+            <li><a href="<?= base_url('indexi'); ?>">services</a></li>
+            <li><a href="<?= base_url('indexi/news/'); ?>">News</a></li>
+            <li><a href="<?= base_url('indexi/about/'); ?>">about</a></li>
         </ul>
-    </header>
+    </nav>
+</header>
